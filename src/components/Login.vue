87ca2f88ff9139317e0b579fb7a0bf494deb34bd
Stewart <7352287+ste-wart@user.noreply.gitee.com>
2021-01-27 21:54:40 +0800

    login-reset

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -7,19 +7,26 @@
              alt="">
       </div>
       <!-- 登录表单区域 -->
-      <el-form label-width="0px" class="login_form">
+      <el-form label-width="0px"
+               class="login_form"
+               ref="loginFormRef"
+               :rules="loginFormReles"
+               :model="loginForm">
         <!-- 用户名 -->
-        <el-form-item>
-          <el-input></el-input>
+        <el-form-item prop="username">
+          <el-input v-model="loginForm.username"
+                    prefix-icon="iconfont icon-user"></el-input>
         </el-form-item>
         <!-- 密码 -->
-        <el-form-item>
-          <el-input></el-input>
+        <el-form-item prop="password">
+          <el-input v-model="loginForm.password"
+                    type="password"
+                    prefix-icon="iconfont icon-3702mima"></el-input>
         </el-form-item>
         <!-- 按钮 -->
         <el-form-item class="btns">
           <el-button type="primary">登录</el-button>
-          <el-button type="info">重置</el-button>
+          <el-button type="info" @click="resetLoginForm">重置</el-button>
         </el-form-item>
       </el-form>
     </div>
@@ -27,7 +34,34 @@
 </template>
 
 <script>
-export default {}
+export default {
+  data() {
+    return {
+      // 登录数据
+      loginForm: {
+        username: 'admin',
+        password: 'admin',
+      },
+      // 表单验证规则
+      loginFormReles: {
+        username: [
+          { required: true, message: '请输入登录名称', trigger: 'blur' },
+          { min: 3, max: 10, message: '长度在 3 到 10 个字符', trigger: 'blur' },
+        ],
+        password: [
+          { required: true, message: '请输入登录密码', trigger: 'blur' },
+          { min: 5, max: 15, message: '长度在 5 到 15 个字符', trigger: 'blur' },
+        ],
+      },
+    }
+  },
+  methods:{
+    // 点击重置登录表单
+    resetLoginForm(){
+      this.$refs.loginFormRef.resetFields()
+    }
+  }
+}
 </script>
 
 <!-- 加上scoped是只有这个单文件组
@@ -66,13 +100,15 @@ export default {}
     }
   }
 }
-.login_form{
+.login_form {
   position: absolute;
   bottom: 0;
   width: 100%;
+  padding: 0 20px;
+  box-sizing: border-box;
 }
-.btns{
+.btns {
   display: flex;
-  justify-content:flex-end;
+  justify-content: flex-end;
 }
 </style>
