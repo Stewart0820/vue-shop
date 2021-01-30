fe44b3611b7387a31cc1957c03754d043024b5a9
Stewart <7352287+ste-wart@user.noreply.gitee.com>
2021-01-30 21:04:49 +0800

    主页部分

--- src/main.js
+++ src/main.js
@@ -9,6 +9,17 @@ import axios from 'axios'
 // 配置请求的根路径
 // axios.defaults.baseURL='https://www.liulongbin.top:8888/api/private/v1/'
 axios.defaults.baseURL = 'http://127.0.0.1:8888/api/private/v1/'
+
+// axios拦截器
+axios.interceptors.request.use(config=>{
+  // 为请求头对象，。添加token验证的authorization字段
+  config.headers.Authorization=window.sessionStorage.getItem('token')
+  // 在最后必须return config
+  return config
+})
+
+
+
 Vue.prototype.$http = axios
 Vue.config.productionTip = false
 
